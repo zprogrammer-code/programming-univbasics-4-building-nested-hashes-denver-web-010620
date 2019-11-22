c0c835b76ded06a49ed55e4c6904c496d3ea642a
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-22 23:52:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier {
 	  :rent_in_dollars => 
 	  {
 	    
-	  }
+	  },
 	}
 	  
 	}
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 	  :rent_in_dollars => 
 	  {
 	    
-	  }
+	  },
 	}
 	  
 	}
