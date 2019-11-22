2493f376b9546da9bb821ac936df3ad9611718a6
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-22 23:57:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,12 @@ def monopoly_with_third_tier
   :railroads => 
   {
 	  :pieces => 4,
-	  :rent_in_dollars => {},
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_piece_owned => 50,
+	    :three_piece_owned => 100,
+	    :four_piece_owned => 200
+	  },
 	}
 	  
 	}
