bcb36ac20d533766fee269a1fce45a25aacd7f94
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-23 00:08:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -74,7 +74,9 @@ def monopoly_with_fourth_tier
 	    :b_and_o_railroad => {
 	        "mortgage_value" => "$400",
 	    },
-	    :shortline_railroad => {}
+	    :shortline_railroad => {
+	      
+	    }
 	  }
 	}
 	  
