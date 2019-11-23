9ff0772abc15b0bbfcf505715e5a9687e48785d5
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-23 00:03:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,9 @@ def monopoly_with_third_tier
 	  },
 	  :names =>
 	  {
-	    :reading_railroad => {},
+	    :reading_railroad => {
+	      
+	    },
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
 	    :shortline_railroad => {}
